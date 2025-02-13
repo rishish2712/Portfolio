a01b06319cc67a06a9f66aa95840b8728ffeed4b
Rishish <rishish2712@gmail.com>
2025-02-13 10:01:09 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -75,14 +75,14 @@ export const projects = [
         title: "Blood Management System",
         des: "Developed a Blood Management System that allows users to donate and request blood, ensuring efficient management and availability. ",
         img: "/bloodbank.png",
-        link:"",
+        link:"visit github",
     },
     {
         id: 2,
         title: "Online Banking System",
         des: "Developed an online banking system allowing users to deposit, withdraw, and check account balances, ensuring secure and efficient transactions.",
         img: "/Bankingproject.png",
-        link:"",
+        link:"visit github",
     },
 ];
 
